8f1150d70d9814c7f7416a8f31e7eafd0f4c9eb8
mohammad maso <mohammadmasoudie@gmail.com>
2022-09-09 07:31:05 +0430

    fix div

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Ⓒ تمامی حقوق محقوظ است.</div>
+      <p className={styles.copyright}>Ⓒ تمامی حقوق محقوظ است.</p>
 
       <div className={styles.settings}>
         {hasMounted && (
